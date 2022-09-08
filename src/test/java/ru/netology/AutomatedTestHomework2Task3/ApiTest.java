2615c619dd661f89c1afe601b378580ace98e59b
Kirill Gureev <kir.gureev2010@ya.ru>
2022-09-08 20:35:17 +0300

    Commit 4. Modified file build.gradle + ApiTest

--- src/test/java/ru/netology/AutomatedTestHomework2Task3/ApiTest.java
+++ src/test/java/ru/netology/AutomatedTestHomework2Task3/ApiTest.java
@@ -1,7 +1,6 @@
 package ru.netology.AutomatedTestHomework2Task3;
 
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
