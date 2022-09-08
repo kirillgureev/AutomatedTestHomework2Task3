374138a1cee983abf18daa088e43430e5050f900
Kirill Gureev <kir.gureev2010@ya.ru>
2022-09-08 20:26:29 +0300

    Commit 2. Add file .yml

--- src/test/java/ru/netology/AutomatedTestHomework2Task3/ApiTest.java
+++ src/test/java/ru/netology/AutomatedTestHomework2Task3/ApiTest.java
@@ -13,14 +13,14 @@ public class ApiTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("Hello, world!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("Hello, World") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello World"))
+                .body("data", equalTo("Hello, World"))
         ;
     }
 }
