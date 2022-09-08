1a1f530ea3e47e3abd20b64e666eb204e433e1b2
Kirill Gureev <kir.gureev2010@ya.ru>
2022-09-08 16:50:29 +0300

    Commit 1. Add code for test

---
+++ src/test/java/ru/netology/AutomatedTestHomework2Task3/ApiTest.java
@@ -0,0 +1,26 @@
+package ru.netology.AutomatedTestHomework2Task3;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ApiTest {
+
+    @Test
+    void shouldPostData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello, world!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello World"))
+        ;
+    }
+}
